4c3dda4982e95c78ca80c07f2d33bf11db24a4c4
SeSAC-Cloud-CTW <slowlight50@gmail.com>
2022-03-21 17:35:12 +0900

    change crawling target url and logic

--- PythonCrawler/main.py
+++ PythonCrawler/main.py
@@ -1,31 +1,44 @@
 import requests
-from apscheduler.schedulers.background import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 from bs4 import BeautifulSoup
+import time
 import os
 import pandas as pd
 
-def get_info(company):
-        info = get_page(company[0])
-        return [company[0], company[1], info[0]]
+df = pd.DataFrame()
+sise = []
 
-def get_page(code):
+def get_info():
+        pages = list(range(1, 21))
+        for idx in pages:
+            get_page(idx)
+
+def get_page(pageNumber):
     # url = 'https://www.google.com/finance/quote/' + code + ':KRX'
-    url = 'https://finance.naver.com/item/main.nhn?code=' + code
+    # url = 'https://finance.naver.com/item/main.nhn?code=' + code
+    url = 'https://finance.naver.com/sise/entryJongmok.naver?&page=' + str(pageNumber)
     result = requests.get(url)
     bs_obj = BeautifulSoup(result.content.decode('euc-kr', 'replace'), "lxml")
-    return [bs_obj.select_one('p.no_today').text.strip().split()[0].replace(',', '')]
+    rows = bs_obj.select('table.type_1 tr')
+    for row in rows[2:-2]:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        sise.append([ele for ele in cols if ele])
 
 def crawl():
-    company_list = []
+    start = time.time()
+    company_dict = {}
     result_list = []
 
     file = pd.read_excel('./data/kospi200.xlsx', usecols="A:B", dtype=str)
 
     for line in file.itertuples(index=False):
-        company_list.append([line[0], line[1]])
+        company_dict[line[1]] = line[0]
 
-    for company in company_list:
-        result_list.append(get_info(company))
+    get_info()
+
+    for info in sise:
+        result_list.append([company_dict[info[0]], info[0], info[1]])
 
     df = pd.DataFrame(list(result_list), columns=['ticker', 'companyName', 'currentPrice'])
     print(df)
@@ -35,10 +48,12 @@ def crawl():
 
     df.to_json('C:/crawled/data/price_now.json',
                orient='records', force_ascii=False)
+    print("timelapse : ", time.time() - start)
 
-sched = BlockingScheduler()               
-    
-sched.add_job(crawl, trigger='cron', second='0', minute='0/5', hour='9-14', day_of_week='mon-fri', month="*")    
-sched.add_job(crawl, trigger='cron', second='0', minute='1-26/5', hour='15', day_of_week='mon-fri', month="*")
+if __name__ == "__main__":
+    sched = BackgroundScheduler()               
+        
+    sched.add_job(crawl, trigger='cron', second='0', minute='0/2', hour='9-14', day_of_week='mon-fri', month="*")    
+    sched.add_job(crawl, trigger='cron', second='0', minute='0-24/2', hour='15', day_of_week='mon-fri', month="*")
 
-sched.start()
+    sched.start()
