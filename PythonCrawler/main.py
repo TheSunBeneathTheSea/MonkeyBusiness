e47ab7d5029b80f95173d78d2c930a9b98895c78
TheSunBeneathTheSea <slowlight50@gmail.com>
2022-10-27 16:13:25 +0900

    기존의 s3 버킷에서 kospi 200 항목 들을 받아 오는 로직 대신 네이버 금융 페이지에서 직접 종목 코드를 파싱하도록 수정 스크래핑한 결과를 s3에 저장하는 부분을 삭제 향후 rest api 형태로 직접 제공하도록 구현할 계획

--- PythonCrawler/main.py
+++ PythonCrawler/main.py
@@ -1,15 +1,9 @@
 import requests
 from apscheduler.schedulers.background import BlockingScheduler
 from bs4 import BeautifulSoup
-import time
-import os
 import pandas as pd
-
-s3 = os.environ.get('s3')
-key = os.environ.get('key')
-secret = os.environ.get('secret')
-
-aws_credentials = { "key": key, "secret": secret }
+import re
+import time
 
 def get_info(sise):
         pages = list(range(1, 21))
@@ -23,40 +17,30 @@ def get_page(pageNumber, sise):
     rows = bs_obj.select('table.type_1 tr')
     for row in rows[2:-2]:
         cols = row.find_all('td')
-        cols = [ele.text.strip() for ele in cols]
-        sise.append([ele for ele in cols if ele])
+        col = [re.search(r'[0-9]{6}', cols[0].contents[0].attrs['href']).group(0)]
+        col = col + [ele.text.strip() for ele in cols]
+        sise.append([ele for ele in col if ele])
 
 def crawl():
-    print("crawling start at: " + time.localtime())
+    print("crawling start at: ", time.localtime())
     start = time.time()
-    df = []
     sise = []
-    company_dict = {}
     result_list = []
-
-    read_path = "s3://" + s3 + '/kospi200.xlsx'
-    file = pd.read_excel(read_path, usecols="A:B", dtype=str, storage_options=aws_credentials)
-
-    for line in file.itertuples(index=False):
-        company_dict[line[1]] = line[0]
 
     get_info(sise)
 
     for info in sise:
-        result_list.append([company_dict[info[0]], info[0], info[1].replace(",", "")])
+        result_list.append([info[0], info[1], info[2].replace(",", ""), info[5], info[6], info[7]])
 
-    df = pd.DataFrame(list(result_list), columns=['ticker', 'companyName', 'currentPrice'])
+    df = pd.DataFrame(list(result_list), columns=['ticker', 'companyName', 'currentPrice', 'volume', 'tradedVolume', 'marketCapitalization'])
     print(df)
 
-    write_path = "s3://" + s3 + '/price_now.json'
-    df.to_json(write_path,
-               orient='records', force_ascii=False, storage_options=aws_credentials)
     print("timelapse : ", time.time() - start)
 
 if __name__ == "__main__":
-    sched = BlockingScheduler({'apscheduler.timezone':'Asia/seoul'})            
-        
-    sched.add_job(crawl, trigger='cron', second='0/10', minute='*', hour='9-14', day_of_week='mon-fri', month="*")    
+    sched = BlockingScheduler({'apscheduler.timezone': 'Asia/seoul'})
+
+    sched.add_job(crawl, trigger='cron', second='0/10', minute='*', hour='9-14', day_of_week='mon-fri', month="*")
     sched.add_job(crawl, trigger='cron', second='0/10', minute='0-30/1', hour='15', day_of_week='mon-fri', month="*")
 
     sched.start()
