f1c11511a7ea7d95ed69553033216b8d054d873f
TheSunBeneathTheSea <slowlight50@gmail.com>
2022-11-10 06:25:12 +0900

    fastapi를 써서 스크래핑 해온 현재 주가 정보를 json 형식으로 보내주는 초간단 api 구현 redis를 써서 일주일치 주가 정보를 저장해두고, 이를 요청하면 보내주는 api를 구현할 계획

--- PythonCrawler/main.py
+++ PythonCrawler/main.py
@@ -1,14 +1,47 @@
 import requests
 from apscheduler.schedulers.background import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 from bs4 import BeautifulSoup
 import pandas as pd
+import redis
 import re
+from fastapi import FastAPI
+import uvicorn
 import time
+import json
+
+
+app = FastAPI()
+ordering = 0
+df = pd.DataFrame()
+scheduler = None
+@app.on_event("startup")
+def boot():
+    global scheduler
+    scheduler= BackgroundScheduler()
+    scheduler.add_job(daily_init, trigger='cron', second='55', minute='59', hour='8', day_of_week='mon-fri', month="*")
+    scheduler.add_job(crawl, trigger='cron', second='0/10', minute='*', hour='9-14', day_of_week='mon-fri', month="*")
+    scheduler.add_job(crawl, trigger='cron', second='0/10', minute='0-30/1', hour='15', day_of_week='mon-fri', month="*")
+
+    # tester
+    # scheduler.add_job(crawl, 'interval', seconds=10)
+    scheduler.start()
+
+@app.get("/")
+def price_now():
+    return json.loads(get_def())
+
+def daily_init():
+    global ordering
+    ordering = 0
+
+def get_def():
+    return df.to_json(orient='records', force_ascii=False)
 
 def get_info(sise):
-        pages = list(range(1, 21))
-        for idx in pages:
-            get_page(idx, sise)
+    pages = list(range(1, 21))
+    for idx in pages:
+        get_page(idx, sise)
 
 def get_page(pageNumber, sise):
     url = 'https://finance.naver.com/sise/entryJongmok.naver?&page=' + str(pageNumber)
@@ -22,7 +55,10 @@ def get_page(pageNumber, sise):
         sise.append([ele for ele in col if ele])
 
 def crawl():
+    global df
+    global ordering
     print("crawling start at: ", time.localtime())
+    ordering = ordering + 1
     start = time.time()
     sise = []
     result_list = []
@@ -30,17 +66,20 @@ def crawl():
     get_info(sise)
 
     for info in sise:
-        result_list.append([info[0], info[1], info[2].replace(",", ""), info[5], info[6], info[7]])
+            result_list.append([info[0], info[1], info[2].replace(",", ""),
+                                info[5].replace(",", ""), info[6].replace(",", ""), info[7].replace(",", "")])
 
-    df = pd.DataFrame(list(result_list), columns=['ticker', 'companyName', 'currentPrice', 'volume', 'tradedVolume', 'marketCapitalization'])
-    print(df)
-
+    df = pd.DataFrame(list(result_list), columns=[u'ticker', 'companyName', 'currentPrice', 'volume', 'tradedVolume',
+                                                   'marketCapitalization'])
+    print(time.strftime('%Y-%m-%d-', time.localtime()) + str(ordering))
+    # print(pd.read_json(df.to_json(orient='split'), orient='split', dtype={'ticker':'str'}))
+    # r.set(time.strftime('%Y-%m-%d:', time.localtime())+str(ordering), df.to_json(orient='split'))
+    # r.expire(time.strftime('%Y-%m-%d:', time.localtime())+str(ordering), 86400)
+    # print(r.get('2022-11-03-1'))
     print("timelapse : ", time.time() - start)
 
-if __name__ == "__main__":
-    sched = BlockingScheduler({'apscheduler.timezone': 'Asia/seoul'})
+if __name__ == '__main__':
+    # r = redis.Redis(host='localhost', port=6379, db=0, username='default', password='password')
+    uvicorn.run("main:app", host='127.0.0.1', port=8000, reload=True)
 
-    sched.add_job(crawl, trigger='cron', second='0/10', minute='*', hour='9-14', day_of_week='mon-fri', month="*")
-    sched.add_job(crawl, trigger='cron', second='0/10', minute='0-30/1', hour='15', day_of_week='mon-fri', month="*")
 
-    sched.start()
